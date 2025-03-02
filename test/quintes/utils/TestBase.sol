de5a5e7982b43c8941963ecb27e7ce2e2820e4a0
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-03-02 10:40:08 +0530

    test: Test base

---
+++ test/quintes/utils/TestBase.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import { Test, console } from "forge-std/Test.sol";
+
+import { Quintes } from "@src/Quintes.sol";
+
+abstract contract TestBase is Test {
+    address public owner;
+    address public minter;
+
+    string public name;
+    string public symbol;
+    uint256 public initialSupply;
+
+    Quintes public quintes;
+
+    function setUp() public {
+        owner = makeAddr("owner");
+        minter = makeAddr("minter");
+
+        name = "Quintes";
+        symbol = "QTS";
+        initialSupply = 6_800_000_000e18;
+
+        Quintes implementation = new Quintes();
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(implementation),
+            owner,
+            abi.encodeWithSelector(Quintes.initialize.selector, name, symbol, owner, initialSupply, owner)
+        );
+        quintes = Quintes(address(proxy));
+
+        vm.startPrank(owner);
+        quintes.grantRole(quintes.getPauserRole(), owner);
+        quintes.grantRole(quintes.getUnpauserRole(), owner);
+        quintes.grantRole(quintes.getBurnerRole(), owner);
+        quintes.grantRole(quintes.getMinterRole(), minter);
+        vm.stopPrank();
+    }
+}
